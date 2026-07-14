d50757f99647a840173703b6a731771054be56cd
Alexsandro Ávila <57649224+AlexVovo@users.noreply.github.com>
2026-07-14 15:53:27 -0300

    Atualizações

--- lib/controllers/projeto_controller.dart
+++ lib/controllers/projeto_controller.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import '../models/projeto_model.dart';
 
 class Projeto {
   final String titulo;
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:file_picker/file_picker.dart';
 import 'package:hub_infobio/views/artigos_page.dart';
-import 'dart:io';
 import 'firebase_options.dart';
 
 void main() async {
